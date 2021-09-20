00882ae8f12e3179dc89fd96c5e86f8217e925d3
MikeStyles <duvankno3@gmail.com>
2021-09-19 22:24:17 -0500

    arreglado para postman

--- src/app.js
+++ src/app.js
@@ -7,15 +7,17 @@ const mongoose = require('mongoose');
 const app = express();
 
 const indexRoutes = require('./routes/index');
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine','ejs');
+app.set('port', process.env.PORT || 4000);
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine','ejs');
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}))
-
+app.use(express.json());
 app.use('/',indexRoutes);
-
+/*app.listen(app.get('port'),()=>{
+    console.log("server on port 3000")
+})*/
 mongoose
     .connect("mongodb+srv://duvanc:H9lmTQIoXDmPOwiV@cluster0.p5egw.mongodb.net/agendamiento?retryWrites=true&w=majority")
     .then(()=>{
