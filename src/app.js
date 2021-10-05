456fd19e30b6f129cc8d94bd3cd32e8ceb9ab0dc
MikeStyles <duvankno3@gmail.com>
2021-10-05 12:48:01 -0500

    ....

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 const indexRoutes = require('./routes/index');
+const { env } = require('process');
 app.set('port', process.env.PORT || 4000);
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine','ejs');
@@ -19,7 +20,7 @@ app.use('/',indexRoutes);
     console.log("server on port 3000")
 })*/
 mongoose
-    .connect("mongodb://duvanc:H9lmTQIoXDmPOwiV@cluster0-shard-00-00.p5egw.mongodb.net:27017,cluster0-shard-00-01.p5egw.mongodb.net:27017,cluster0-shard-00-02.p5egw.mongodb.net:27017/agendamiento?ssl=true&replicaSet=atlas-14dpig-shard-0&authSource=admin&retryWrites=true&w=majority")
+    .connect(env)
     .then(()=>{
         app.listen(app.get('port'),()=>{
             console.log(`server on port ${app.get('port')}`)
