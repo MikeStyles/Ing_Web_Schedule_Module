dcc908a546f8e2e04d84b6e7533f48331409bca6
MikeStyles <duvankno3@gmail.com>
2021-10-07 17:20:15 -0500

    Add cors

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const config = require('./config');
 const app = express();
-
+const cors = require('cors');
 const indexRoutes = require('./routes/schedule.routes');
 const conexionDB = require('./db.conexion');
 app.set('port', process.env.PORT || 4000);
 
 conexionDB();
-
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
